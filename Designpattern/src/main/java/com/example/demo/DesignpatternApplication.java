1e1aeacd7ca2291364c6288a66f4c9e9425954f5
chineseP <yqxz045000@users.noreply.github.com>
2017-09-07 19:52:40 +0800

    对项目做一些修改
    对项目做一些修改description

--- Designpattern/src/main/java/com/example/demo/DesignpatternApplication.java
+++ Designpattern/src/main/java/com/example/demo/DesignpatternApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DesignpatternApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) {1111111111111111111
 		SpringApplication.run(DesignpatternApplication.class, args);
 	}
 }
